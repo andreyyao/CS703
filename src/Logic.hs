caa5ddec3ef8c5d1dacfaef95d221d63882bc854
Gabe <gabeorlanski@gmail.com>
2023-12-02 13:24:45 -0600

    Logic Initial Rules

--- src/Logic.hs
+++ src/Logic.hs
@@ -14,26 +14,22 @@ data Inference
   | DoubleNegation
   -- | ...
 
-data Conclusion = Pair (Derivation, Prop)
+data Conclusion = Derivation * Prop
 data Derivation
   = Binary Inference Conclusion Conclusion -- The two derivations are the two trees in the premise, and the Prop is the conclusion
   | Unary Inference Conclusion -- One premise, e.g. Double negation elimination
-  | Axiom
+  | Axiom 
 
 -- For example, we start with an (Axiom, P) conclusion for every type T translated in the context translated to P
 
 -- This terminates when we found the "proposition" representing the type of the hole
 
--- doubleNegation :: Prop -> Derivation
--- doubleNegation (Neg (Neg p)) = (Unary DoubleNegation (Unary DoubleNegation (Pair (Axiom, p))))
 
-getDerivation :: Derivation -> Prop -> Derivation
-getDerivation d p = case p of
-  (Neg (Neg p)) -> (Unary DoubleNegation (Pair (d,p)))
+generateHelper :: Derivation -> Prop -> Conclusion
+generateHelper d p = case p of
+  (Neg (Neg p)) -> (Unary DoubleNegation d, p)
   _ -> error "Not implemented"
-
--- generateTree :: [Derivation] -> Prop -> [Derivation]
--- generateTree derivations holeProp =
---   for d in derivation:
---   match d's conclusion
---   | (Neg (Neg P)) -> add (Unary DoubleNegation d P) to current list of derivations
+-- generateTree :: Conclusion -> Conclusion
+-- generateTree d = case d of
+--   (Axiom (Neg (Neg p))) -> 
+--   _ -> error "Not implemented"
