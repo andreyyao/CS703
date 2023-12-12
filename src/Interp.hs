62af43b1c23ac4643f34d66ececcc56a6ddff4b0
Andrey Yao <awy32@cornell.edu>
2023-12-11 23:51:57 -0600

    I think interpreter is working with Callcc?

--- src/Interp.hs
+++ src/Interp.hs
@@ -1,35 +1,35 @@
 module Interp(interp, Value) where
 import qualified Data.Map as Map
+import Debug.Trace
 import Ast
 
 -- The interpreter is implemented as a shallow embedding into the Haskell semantics
 
-type Lamb = (String, Tipe, Expr)
-
 data Value
-  = Closure Lamb Env -- A lambda paired with an enrivonment
-  | Konstant Constant
+  = VClosure Kont -- A lambda paired with an enrivonment
+  | VConstant Constant
   | VPair Value Value -- It could be a pair of closures or something
 
 type Env = Map.Map String Value
 
+-- This is like the evaluation context
 type Kont = Value -> Env -> Value
 
 eval :: Expr -> Env -> Kont -> Value
 eval expr env k =
   case expr of
-    Const c -> k (Konstant c) env
+    Const c -> k (VConstant c) env
     Var x ->
       case Map.lookup x env of
         Just v -> k v env
         _ -> error "Unbound variable"
     Binary e1 op e2 -> eval e1 env $ \v1 env' -> eval e2 env' $ \v2 env'' -> case (v1, v2) of
-      (Konstant (ConstInt a), Konstant (ConstInt b)) ->
+      (VConstant (ConstInt a), VConstant (ConstInt b)) ->
        let res = case op of
             Add -> a + b
             Mul -> a * b
             Sub -> a - b in
-        k (Konstant $ ConstInt res) env''
+        k (VConstant $ ConstInt res) env''
       _ -> error "Invalid addition"
     Projl e -> eval e env $ \v env' -> case v of
       VPair v1 _ -> k v1 env'
@@ -38,15 +38,17 @@ eval expr env k =
       VPair _ v2 -> k v2 env'
       _ -> error "hehe"
     Pair e1 e2 -> eval e1 env $ \v1 env' -> eval e2 env' $ \v2 env'' -> k (VPair v1 v2) env''
-    Branch b e1 e2 -> eval b env $ \v env' -> case v of
-      Konstant (ConstBool cond) -> if cond then eval e1 env k else eval e2 env k
+    Branch b e1 e2 -> eval b env $ \v _ -> case v of
+      VConstant (ConstBool cond) -> if cond then eval e1 env k else eval e2 env k
       _ -> error "hehe"
-    Lambda x t e -> k (Closure (x, t, e) env) env
-    App e1 e2 -> eval e1 env $ \v1 env' -> eval e2 env' $ \v2 env'' -> case v1 of
-      Closure (x, _, e) env''' -> eval e (Map.insert x v2 env''') k
+    Lambda x _ e -> VClosure (\v' env' -> eval e (Map.insert x v' env) k)
+    App e1 e2 -> eval e1 env $ \v1 env' -> eval e2 env' $ \v2 _ -> case traceShowId v1 of
+      VClosure k' -> k' v2 env
       _ -> error "hehe"
     Let x e1 e2 -> eval e1 env $ \v1 env' -> eval e2 (Map.insert x v1 env') k
-    Callcc e -> error "unimplemented"
+    Callcc e -> case eval e env const of
+      VClosure k' -> k' (VClosure k) env
+      _ -> error "hehe"
     Abort _ e -> eval e env const
     Hole _ -> error "Encountered hole during interpretation"
 
@@ -55,6 +57,6 @@ interp expr = eval expr Map.empty const
 
 instance Show Value where
   show val = case val of
-    Closure (x, t, e) _ -> "closure<<" ++ show (Lambda x t e) ++ ">>"
-    Konstant c -> show c
+    VClosure _ -> "<closure>"
+    VConstant c -> show c
     VPair v1 v2 -> "(" ++ show v1 ++ ", " ++ show v2 ++ ")"
--- test/Spec.hs
+++ test/Spec.hs
@@ -12,45 +12,44 @@ import Ast(Tipe)
 import Logic()
 import Synthesizer(synthesize)
 
-checkSourceCode :: String -> IO ()
-checkSourceCode prog =
-  let divider = "\n------------------------" in do
+checkSourceCode :: (String, String) -> IO ()
+checkSourceCode (file, prog) =
+  let divider = "\n---------------" ++ file ++ "----------------" in do
     putStrLn (divider ++ "\nProg: " ++ prog);
     case (typecheck . parseExpr . scanMany) prog of
-      Just t -> putStrLn ("Type: " ++ show t ++ divider)
+      Just t -> putStrLn ("Type: " ++ show t)
       Nothing -> putStrLn "Error checking program"
 
-checkSynthesize :: String -> IO ()
-checkSynthesize prog =
-  let divider = "\n------------------------" in do
+checkSynthesize :: (String, String) -> IO ()
+checkSynthesize (file, prog) =
+  let divider = "\n---------------" ++ file ++ "----------------" in do
     putStrLn (divider ++ "\nProg: " ++ prog);
     case (synthesize . parseExpr . scanMany) prog of
-      Just e -> putStrLn ("Synthesized: " ++ show e ++ divider)
+      Just e -> putStrLn ("Synthesized: " ++ show e)
       Nothing -> putStrLn "Error synthesizing program"
 
-checkSynthInterp :: String -> IO ()
-checkSynthInterp prog =
-  let divider = "\n------------------------" in do
+checkSynthInterp :: (String, String) -> IO ()
+checkSynthInterp (file, prog) =
+  let divider = "\n---------------" ++ file ++ "----------------" in do
     putStrLn (divider ++ "\nProg: " ++ prog);
     case (synthesize . parseExpr . scanMany) prog of
-      Just e -> putStrLn ("Interpreted: " ++ show (interp e) ++ divider)
+      Just e -> putStrLn ("Interpreted: " ++ show (interp e))
       Nothing -> putStrLn "Error interpreting program"
 
-readFilesInDirectory :: IO [String]
+readFilesInDirectory :: IO [(String, String)]
 readFilesInDirectory =
   let dir = "examples" in do
     files <- listDirectory dir
     let filePaths = map (dir </>) files
     mapM readFileAsStrings filePaths
 
-readFileAsStrings :: FilePath -> IO String
+readFileAsStrings :: FilePath -> IO (String, String)
 readFileAsStrings filePath = do
     fileContent <- TIO.readFile filePath
-    return (T.unpack fileContent)
+    return (filePath, T.unpack fileContent)
 
 main :: IO ()
 main = do
   programs <- readFilesInDirectory
-  mapM_ checkSourceCode programs
   mapM_ checkSynthesize programs
   mapM_ checkSynthInterp programs
